413c82a8da6a0833287bd142756170cfe0cc782d
孟世博 <897205285@qq.com>
2023-04-24 15:10:51 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,73 @@
 import fetch from "node-fetch";
-import { readFile, writeFile } from "fs";
+import { readFile, writeFile } from "fs/promises";
 import { uniqBy } from "lodash";
 import dayjs from "dayjs";
 import path from "path";
 import "dayjs/locale/zh-cn";
 dayjs.locale("zh-cn");
 
-const cwd = process.cwd();
+const fetchComments = async () => {
+  const response = await fetch(
+    `https://m.weibo.cn/api/comments/show?id=Is9M7taaY&page=0`,
+    {}
+  );
+  const commentJson = await response.json();
+  return commentJson?.data?.data;
+};
+
+const processComments = (commentData) => {
+  return commentData.map((item) => {
+    const newTime = dayjs().format("YYYY-MM-DD A hh:mm dddd");
+    let recordTime = newTime;
+
+    if (item.created_at.includes("分")) {
+      const time = item.created_at.split("分")[0];
+      recordTime = dayjs()
+        .subtract(time, "minute")
+        .format("YYYY-MM-DD A hh:mm dddd");
+    }
+
+    if (item.created_at.includes("小时")) {
+      const time = item.created_at.split("小时")[0];
+      recordTime = dayjs()
+        .subtract(time, "hour")
+        .format("YYYY-MM-DD A hh:mm dddd");
+    }
+
+    return { ...item, recordTime };
+  });
+};
+
+const readDataFile = async (filePath) => {
+  try {
+    const fileData = await readFile(filePath);
+    return JSON.parse(fileData.toString());
+  } catch (err) {
+    return null;
+  }
+};
+
+const writeDataFile = async (filePath, data) => {
+  await writeFile(filePath, JSON.stringify(data));
+};
 
 const init = async () => {
   const date = dayjs().format("YYYY-MM-DD");
   const fileName = `${date}.json`;
   const filePath = path.resolve(__dirname, `../data/${fileName}`);
 
-  const comment = await fetch(
-    `https://m.weibo.cn/api/comments/show?id=Is9M7taaY&page=0`,
-    {}
-  );
+  const commentData = await fetchComments();
+  const processedComments = processComments(commentData);
 
-  const commentJson = await comment.json();
+  const existingData = await readDataFile(filePath);
+  let newData = processedComments;
 
-  const commentData = commentJson?.data?.data;
+  if (existingData) {
+    newData = uniqBy([...existingData, ...processedComments], "id");
+    newData.sort((a, b) => a.id - b.id);
+  }
 
-  commentData.forEach((item, index) => {
-    const newTime = dayjs().format("YYYY-MM-DD A hh:mm dddd");
-    commentData[index].recordTime = newTime;
-
-    if (item.created_at.indexOf("分") >= 0) {
-      const time = item.created_at?.split("分")[0];
-      if (time) {
-        const newTime = dayjs()
-          .subtract(time, "minute")
-          .format("YYYY-MM-DD A hh:mm dddd");
-        commentData[index].originTime = dayjs().subtract(time, "minute");
-        commentData[index].recordTime = newTime;
-      }
-    }
-
-    if (item.created_at.indexOf("小时") >= 0) {
-      const time = item.created_at?.split("小时")[0];
-      if (time) {
-        const newTime = dayjs()
-          .subtract(time, "hour")
-          .format("YYYY-MM-DD A hh:mm dddd");
-        commentData[index].originTime = dayjs().subtract(time, "hour");
-        commentData[index].recordTime = newTime;
-      }
-    }
-  });
-
-  readFile(filePath, function (err, fileData) {
-    if (fileData) {
-      // if file exist then read file and append data
-      const beforeString = fileData.toString();
-      const beforeData = JSON.parse(beforeString);
-
-      let newComment = [...beforeData, ...commentData];
-
-      // remove duplicate
-      newComment = uniqBy(newComment, "id");
-
-      // sort by id
-      newComment = newComment.sort((a, b) => {
-        return a.id - b.id;
-      });
-
-      console.log(`数据 ${newComment.length}`);
-
-      if (newComment && newComment.length) {
-        writeFile(filePath, JSON.stringify(newComment), function (err) {
-          if (err) {
-            return console.error(err);
-          }
-        });
-      }
-    } else {
-      // if file not exist then create file and write data
-      writeFile(filePath, JSON.stringify([...commentData]), function (err) {
-        if (err) {
-          return console.error(err);
-        }
-      });
-    }
-  });
+  await writeDataFile(filePath, newData);
 };
 
 init();
